159db4224948ba3a538e1f58747a53c523636fa2
emeenent <chukwuwmekaoguejiofor14@gmail.com>
2025-03-14 23:15:24 +0100

    accounts/view.py: added logic for retrieving, creating and updating user profile

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, RegisterSerializer, ProfileSerializer
+from .models import Profile
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    """
+    API endpoint for user registration
+    """
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    """
+    API endpoint to get user details
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self):
+        return self.request.user
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    """
+    API endpoint to get and update user profile
+    """
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
