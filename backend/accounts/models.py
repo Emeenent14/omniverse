ca75fe6e26bfbf5bce7af8726c74702a1e956dc4
emeenent <chukwuwmekaoguejiofor14@gmail.com>
2025-03-14 20:40:35 +0100

    accounts/models.py Extended user profile model to store additional users information

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    """
+    Extended user profile model that stores additional user information
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    avatar = models.ImageField(upload_to='profile_avatars/', blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
