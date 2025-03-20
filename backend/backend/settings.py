ce62e2951c17e9d180cc50915ab17ed6f86081ec
emeenent <chukwuemekaoguejiofor14@gmail.com>
2025-03-20 23:28:21 +0100

    created superuser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party apps
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
     'rest_framework',
     'corsheaders',
     # Custom apps
